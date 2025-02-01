f19c9cd97979d548a94f2e82539b388e9f87affa
hugoas232 <162774066+hugoas232@users.noreply.github.com>
2025-02-01 15:23:44 -0600

    Add files via upload

---
+++ Python-Platformer-main/Tiled/Main Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Main Tileset" tilewidth="48" tileheight="48" tilecount="21" columns="7">
+ <image source="../assets/Terrain/Terrain.png" width="352" height="176"/>
+</tileset>
